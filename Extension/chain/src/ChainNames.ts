b2234c74d6d84cf8971f6a9cd4868a2a9ba942f7
Shun Kakinoki <shunkakinoki@gmail.com>
2022-11-23 09:21:47 -0800

    perf: ⚡️ ini avalanche and bsc

--- Extension/chain/src/ChainNames.ts
+++ Extension/chain/src/ChainNames.ts
@@ -2,6 +2,8 @@ export const ChainNames = {
   "0x1": "Ethereum",
   "0x5": "Goerli",
   "0xa": "Optimism",
+  "0x38": "BSC",
   "0x89": "Polygon",
   "0xa4b1": "Arbitrum",
+  "0xa86a": "Avalanche",
 };
