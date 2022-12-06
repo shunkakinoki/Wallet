7ef5f79236a186f72842328c7059527115924715
Shun Kakinoki <shunkakinoki@gmail.com>
2022-12-05 18:01:58 -0800

    fix: 🐛 gas estimation

--- Extension/content/src/hooks/useGasEstimation.ts
+++ Extension/content/src/hooks/useGasEstimation.ts
@@ -1,25 +1,50 @@
 import useSWR from "swr";
 
 const fetcher = params => {
+  if (!params.data) {
+    return "0x5208";
+  }
   if (window.ethereum.isStorybook) {
     return "0x69";
   }
   return window.ethereum.rpc
     .call({
       jsonrpc: "2.0",
-      method: "eth_estimateGas",
-      params: [
-        {
-          from: params.from,
-          to: params.to,
-          data: params.data,
-          value: params?.value ?? "0x0",
-        },
-      ],
+      method: "eth_getBlockByNumber",
+      params: ["latest", true],
       id: "1",
     })
-    .then(response => {
-      return response.result;
+    .then(res => {
+      const blockGasLimit = res.result.gasLimit
+        ? parseInt(res.result.gasLimit, 16)
+        : 90_000;
+      window.ethereum.rpc
+        .call({
+          jsonrpc: "2.0",
+          method: "eth_estimateGas",
+          params: [
+            {
+              to: params.to,
+            },
+          ],
+          id: "1",
+        })
+        .then(response => {
+          if (!response.result) {
+            const saferGasLimit = blockGasLimit * 0.95;
+            return saferGasLimit.toString(16);
+          }
+          const estimatedGasLimit = parseInt(response.result, 16);
+          const bufferedGasLimit = parseInt(response.result, 16) * 1.5;
+          const upperGasLimitBn = blockGasLimit * 0.9;
+          if (estimatedGasLimit > upperGasLimitBn) {
+            return estimatedGasLimit.toString(16);
+          }
+          if (bufferedGasLimit > upperGasLimitBn) {
+            return bufferedGasLimit.toString(16);
+          }
+          return upperGasLimitBn.toString(16);
+        });
     });
 };
 
@@ -37,7 +62,7 @@ export const useGasEstimation = params => {
   );
 
   return {
-    gasEstimation: !error ? data : "0x5208",
+    gasEstimation: !error ? data : "0x15f90",
     error,
     isLoading,
     isValidating,
