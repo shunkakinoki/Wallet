a06fabf4adcf9a3973de080c7c842cc398928b9f
Shun Kakinoki <shunkakinoki@gmail.com>
2022-12-21 23:14:22 -0800

    feat: 🎸 ini style module

--- Extension/popup/src/pages/store.tsx
+++ Extension/popup/src/pages/store.tsx
@@ -12,6 +12,8 @@ import { AnimatePresence, motion, LayoutGroup } from "framer-motion";
 import Link from "next/link";
 import { useState } from "react";
 
+import s from "./store.module.css";
+
 const supportLinks = [
   {
     name: "New",
@@ -65,46 +67,60 @@ export default function Store() {
   let [hoveredIndex, setHoveredIndex] = useState<number>(0);
 
   return (
-    <div className="flex min-w-[320px] justify-center">
-      <div className="w-full max-w-lg overflow-x-scroll">
-        <nav className="mx-4 mt-11 flex gap-8">
-          <LayoutGroup id="nav">
-            {supportLinks.map((link, index) => {
-              return (
-                <Link
-                  key={link.href}
-                  href={link.href}
-                  className={clsx(
-                    "relative -my-2 -mx-3 rounded-lg px-3 py-2 text-sm text-gray-700 transition-colors delay-150 hover:text-gray-900 hover:delay-[0ms]",
-                    hoveredIndex === index && "bg-gray-200",
-                  )}
-                  onMouseEnter={() => {
-                    return setHoveredIndex(index);
-                  }}
-                >
-                  <AnimatePresence>
-                    {hoveredIndex === index && (
-                      <motion.span
-                        className="absolute inset-0 rounded-lg bg-gray-100"
-                        layoutId="hoverTab"
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1, transition: { duration: 0.15 } }}
-                        exit={{
-                          opacity: 0,
-                          transition: { duration: 0.15, delay: 0.2 },
-                        }}
-                      />
+    <div className="flex justify-center">
+      <div className="max-w-lg flex-col">
+        <div
+          className={clsx(
+            "flex w-full overflow-x-scroll",
+            hoveredIndex < 3
+              ? s.left
+              : hoveredIndex === supportLinks.length - 1
+              ? s.right
+              : s.scroll,
+          )}
+        >
+          <nav className="mx-4 my-11 flex gap-8">
+            <LayoutGroup id="nav">
+              {supportLinks.map((link, index) => {
+                return (
+                  <Link
+                    key={link.href}
+                    href={link.href}
+                    className={clsx(
+                      "relative -my-2 -mx-3 rounded-lg px-3 py-2 text-sm text-gray-700 transition-colors delay-150 hover:text-gray-900 hover:delay-[0ms]",
+                      hoveredIndex === index && "bg-gray-200",
                     )}
-                  </AnimatePresence>
-                  <div className="relative z-10 flex items-center">
-                    <link.icon className="mr-2 h-4 w-4" aria-hidden="true" />
-                    {link.name}
-                  </div>
-                </Link>
-              );
-            })}
-          </LayoutGroup>
-        </nav>
+                    onMouseEnter={() => {
+                      return setHoveredIndex(index);
+                    }}
+                  >
+                    <AnimatePresence>
+                      {hoveredIndex === index && (
+                        <motion.span
+                          className="absolute inset-0 rounded-lg bg-gray-100"
+                          layoutId="hoverTab"
+                          initial={{ opacity: 0 }}
+                          animate={{
+                            opacity: 1,
+                            transition: { duration: 0.15 },
+                          }}
+                          exit={{
+                            opacity: 0,
+                            transition: { duration: 0.15, delay: 0.2 },
+                          }}
+                        />
+                      )}
+                    </AnimatePresence>
+                    <div className="relative z-10 flex items-center">
+                      <link.icon className="mr-2 h-4 w-4" aria-hidden="true" />
+                      {link.name}
+                    </div>
+                  </Link>
+                );
+              })}
+            </LayoutGroup>
+          </nav>
+        </div>
         <main className="mt-12 w-full">
           <AnimatePresence mode="popLayout">
             <motion.div
