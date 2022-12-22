01784a5a28c8577256a1dc420bfcc4ba619c8603
Shun Kakinoki <shunkakinoki@gmail.com>
2022-12-21 22:24:27 -0800

    feat: 🎸 ini support links

--- Extension/popup/src/pages/store.tsx
+++ Extension/popup/src/pages/store.tsx
@@ -1,26 +1,79 @@
+import {
+  BeakerIcon,
+  BriefcaseIcon,
+  BuildingStorefrontIcon,
+  FaceSmileIcon,
+  CubeTransparentIcon,
+  FireIcon,
+  ScaleIcon,
+} from "@heroicons/react/24/outline";
 import clsx from "clsx";
 import { AnimatePresence, motion, LayoutGroup } from "framer-motion";
 import Link from "next/link";
 import { useState } from "react";
+
+const supportLinks = [
+  {
+    name: "New",
+    href: "#",
+    description: "New Dapps to try out across the ecosystem.",
+    icon: BeakerIcon,
+  },
+  {
+    name: "Trending",
+    href: "#",
+    description: "Trending Dapps curated from Light users.",
+    icon: FireIcon,
+  },
+  {
+    name: "Bridge",
+    href: "#",
+    description:
+      "Move assets between different layers of Ethereum by bridging them across.",
+    icon: BriefcaseIcon,
+  },
+  {
+    name: "Mint",
+    href: "#",
+    description:
+      "Move assets between different layers of Ethereum by bridging them across.",
+    icon: CubeTransparentIcon,
+  },
+  {
+    name: "NFT",
+    href: "#",
+    description: "Place bids on existing NFTs that you want to buy.",
+    icon: BuildingStorefrontIcon,
+  },
+  {
+    name: "Swap",
+    href: "#",
+    description:
+      "Swap tokens on Ethereum & other networks using decentralized exchanges, which are known as DEXs.",
+    icon: ScaleIcon,
+  },
+  {
+    name: "Social",
+    href: "#",
+    description:
+      "Interact with next-generation of blockchain social apps that assures your data ownership.",
+    icon: FaceSmileIcon,
+  },
+];
 
 export default function Store() {
   let [hoveredIndex, setHoveredIndex] = useState<number>(0);
 
   return (
     <div className="flex min-w-[320px] justify-center">
-      <div className="w-full max-w-lg">
-        <nav className="mt-11 flex gap-8">
+      <div className="w-full max-w-lg overflow-x-scroll">
+        <nav className="mx-4 mt-11 flex gap-8">
           <LayoutGroup id="nav">
-            {[
-              ["New To Dapps", "#features"],
-              ["Trending", "#reviews"],
-              ["Exchanges", "#pricing"],
-              ["NFT Marketplaces", "#faqs"],
-            ].map(([label, href], index) => {
+            {supportLinks.map((link, index) => {
               return (
                 <Link
-                  key={label}
-                  href={href}
+                  key={link.href}
+                  href={link.href}
                   className={clsx(
                     "relative -my-2 -mx-3 rounded-lg px-3 py-2 text-sm text-gray-700 transition-colors delay-150 hover:text-gray-900 hover:delay-[0ms]",
                     hoveredIndex === index && "bg-gray-200",
@@ -43,23 +96,26 @@ export default function Store() {
                       />
                     )}
                   </AnimatePresence>
-                  <span className="relative z-10">{label}</span>
+                  <div className="relative z-10 flex items-center">
+                    <link.icon className="mr-2 h-4 w-4" aria-hidden="true" />
+                    {link.name}
+                  </div>
                 </Link>
               );
             })}
           </LayoutGroup>
         </nav>
-        <main className="w-full">
+        <main className="mt-12 w-full">
           <AnimatePresence>
             <motion.div
-              key={hoveredIndex ? "ssdf" : "empty"}
-              className="text-3xl"
+              key={supportLinks[hoveredIndex].name}
+              className="text-lg text-gray-600"
               initial={{ y: 10, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
               exit={{ y: -10, opacity: 0 }}
               transition={{ duration: 0.2 }}
             >
-              {hoveredIndex === 1 ? "asdf" : "😋"}
+              {supportLinks[hoveredIndex].description}
             </motion.div>
           </AnimatePresence>
         </main>
